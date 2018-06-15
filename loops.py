3f729dcd601ad55d92dfe59c2b4cdea2fc82f7e7
elizamwikali <mwikalielizabeth6@gmail.com>
2018-06-15 10:00:54 +0300

    loops commit

---
+++ loops.py
@@ -0,0 +1,65 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> numbers = [1,2,3,4,5,6,7,8,9]
+>>> for number in numbers:
+	print(number)
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> numbers = [1,2,3,4,5,6,7,8,9,10]
+>>> for number in numbers:
+	if number >3:
+		print(number)
+
+		
+4
+5
+6
+7
+8
+9
+10
+>>> letters = ['a','b','c','d','e','f','g','h']
+>>> for x in letters:
+	print(x)
+
+	
+a
+b
+c
+d
+e
+f
+g
+h
+>>> names = ['eliza','ashley','rania','hannah','pollet','bigeni']
+>>> for friends in names:
+	print(friends)
+
+	
+eliza
+ashley
+rania
+hannah
+pollet
+bigeni
+>>> laptops = ['Lenovo','Apple','HP','Microsoft','Dell','Acer']
+>>> for brands in laptops:
+	print(brands)
+
+	
+Lenovo
+Apple
+HP
+Microsoft
+Dell
+Acer
+>>> 
---
+++ oop.py
@@ -0,0 +1,11 @@
+class person:
+    def__init__(self,name,age,height,weight):
+    self.name = name
+    self.age = age
+    self.height = height
+    self.weight = weight
+
+    eliza = person("eliza",20,"5'3","60kgs")
+    print(eliza.age)
+
+
